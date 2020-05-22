2f84d3f4b1883cd85fbbd17bc67fcba1c1c22f33
DevShaded <devshaded@gmail.com>
2020-05-23 00:02:55 +0200

    Add: paragraph command

---
+++ Commands/HTML/paragraphs.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+    name: 'paragraphs',
+    description: 'How to: HTML paragraph tag',
+    execute: async function (client, message, args) {
+
+        await message.channel.send({
+            embed: {
+                color: '#14b1f5',
+                title: 'HTML paragraphs',
+                description: `**Definition and Usage**\n` +
+                    "\n" +
+                    " The `<p>` tag defines a paragraph.\n" +
+                    "\n" +
+                    "Browsers automatically add a single blank line before and after each `<p>` element.\n" +
+                    "\n" +
+                    `**Example**` +
+                    `\`\`\`HTML\n<body>\n    <p>This is some text in a paragraph.</p>\n</body>\`\`\``,
+            }
+        });
+    }
+};
