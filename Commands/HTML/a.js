da2e61f1f650ce86028a979ecc2e091452288677
DevShaded <devshaded@gmail.com>
2020-05-22 21:46:13 +0200

    Add: how to add links HTML

---
+++ Commands/HTML/a.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    name: 'links',
+    description: 'How to: HTML anchor tag',
+    execute: async function (client, message, args) {
+
+        await message.channel.send({
+            embed: {
+                color: '#14b1f5',
+                title: 'HTML links',
+                description: `**Definition and Usage**\n` +
+                    "\n" +
+                    " The `<a>` tag defines a hyperlink, which is used to link from one page to another.\n" +
+                    "\n" +
+                    "The most important attribute of the `<a>` element is the href attribute, which indicates the link's destination.\n" +
+                    "\n" +
+                    "By default, links will appear as follows in all browsers:\n" +
+                    "• An unvisited link is underlined and blue\n" +
+                    "• A visited link is underlined and purple\n" +
+                    "• An active link is underlined and red\n" +
+                    "\n" +
+                    `**Example**` +
+                    `\`\`\`HTML\n<body>\n    <a href="https://www.google.com">Visit Google!</a>\n</body>\`\`\``,
+            }
+        });
+    }
+};
