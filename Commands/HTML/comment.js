7ecdbd62ed869de73c2e8efae41e614b4196c2e2
DevShaded <devshaded@gmail.com>
2020-05-22 21:50:50 +0200

    edit: changed something

--- Commands/HTML/comment.js
+++ Commands/HTML/comment.js
@@ -7,16 +7,16 @@ module.exports = {
 
         await message.channel.send({
             embed: {
-                color: '#14b1f5',
-                title: 'HTML Comments',
-                description: `**Definition and Usage**\n` +
-                    "\n" +
-                    " The comment tag is used to insert comments in the source code. Comments are not displayed in the browsers.\n" +
-                    "\n" +
-                    "You can use comments to explain your code, which can help you when you edit the source code at a later date. This is especially useful if you have a lot of code.\n" +
-                    "\n" +
-                    `**Example**` +
-                    `\`\`\`HTML\n<body>\n<!--This is a comment-->\n<p>This is a paragraph.</p>\n</body>\`\`\``,
+            color: '#14b1f5',
+            title: 'HTML Comments',
+            description: `**Definition and Usage**\n` +
+                "\n" +
+                " The comment tag is used to insert comments in the source code. Comments are not displayed in the browsers.\n" +
+                "\n" +
+                "You can use comments to explain your code, which can help you when you edit the source code at a later date. This is especially useful if you have a lot of code.\n" +
+                "\n" +
+                `**Example**` +
+                    `\`\`\`HTML\n<body>\n  <!--This is a comment-->\n  <p>This is a paragraph.</p>\n</body>\`\`\``,
             }
         });
     }
