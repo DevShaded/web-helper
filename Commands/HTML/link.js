3c64c6523c44998dfe04cc32b92e3157b6edb6e7
DevShaded <devshaded@gmail.com>
2020-06-03 09:16:33 +0200

    Edit: Fixed so it says <link> not link>

--- Commands/HTML/link.js
+++ Commands/HTML/link.js
@@ -11,9 +11,9 @@ module.exports = {
                 title: 'HTML link',
                 description: `**Definition and Usage**\n` +
                     "\n" +
-                    " The `link>` tag defines the relationship between the current document and an external resource.\n" +
+                    " The `<link>` tag defines the relationship between the current document and an external resource.\n" +
                     "\n" +
-                    "The `link>` tag is most often used to link to external style sheets.\n" +
+                    "The `<link>` tag is most often used to link to external style sheets.\n" +
                     "\n" +
                     "The `<link>` element is an empty element, it contains attributes only.\n" +
                     "\n" +
