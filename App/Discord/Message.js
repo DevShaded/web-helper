ad6ccddb397c4b19655d30ab8c8b42809271e3ee
DevShaded <devshaded@gmail.com>
2020-06-20 23:27:13 +0200

    Add: Tracks guilds and stores in DB

--- App/Discord/Message.js
+++ App/Discord/Message.js
@@ -4,10 +4,39 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { pool } = require('../DB');
+const moment = require('moment');
 
 function Message() {
 
 }
+
+Message.prototype.createGuild = async function (message) {
+    let createdDatetime = message.guild.createdTimestamp;
+    createdDatetime = moment(createdDatetime).format('YYYY-MM-DD HH-mm-ss');
+
+    await pool.execute("INSERT INTO guilds (guildName, guildID, banner, deleted, description, explicitContentFilter, iconURL, large, memberCount, mfaLevel, ownerID, partnered, premiumSubscriptionCount, premiumTier, region, splashURL, vanityURLCode, verificationLevel, verified, createdTimestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+        message.guild.name,
+        message.guild.id,
+        message.guild.banner,
+        message.guild.deleted,
+        message.guild.description,
+        message.guild.explicitContentFilter,
+        message.guild.iconURL(),
+        message.guild.large,
+        message.guild.memberCount,
+        message.guild.mfaLevel,
+        message.guild.ownerID,
+        message.guild.partnered,
+        message.guild.premiumSubscriptionCount,
+        message.guild.premiumTier,
+        message.guild.region,
+        message.guild.splashURL(),
+        message.guild.vanityURLCode,
+        message.guild.verificationLevel,
+        message.guild.verified,
+        createdDatetime
+    ]);
+};
 
 /**
  * What to do when a new <message> has been sent
